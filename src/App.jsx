056d1e58aad7d0daead8484bca6035108deb00d5
rami <rami_zezo1986@hotmail.com>
2024-01-17 09:52:27 +0100

    Live coding useparams

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { Link } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import UserDetails from './pages/UserDetails'
 
 function App() {
 
@@ -16,6 +18,11 @@ function App() {
           <Link to='/user/2'>User 2</Link>
         </li>
       </ul>
+      <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/user/:userId' element={<UserDetails />}/>
+
+      </Routes>
       
     </>
   )
--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import usersData from '../data/users.json';
+import { useParams } from 'react-router-dom';
 
 function UserDetails() {
+  const {userId} = useParams();
+  const user = usersData.users.find((user)=> user.id === parseInt(userId));
+  
   return (
+    <>
     <div>UserDetails</div>
+    <p>User Id : {user.id}</p>
+    <p>Name : {user.name}</p>
+    <p>Email : {user.email}</p>
+
+    </>
   )
 }
 
